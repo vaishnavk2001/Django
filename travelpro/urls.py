c3e60050c9471dc0b1093f98220c1c9f243766a7
vaishnav janardhanan <vaishnavjanardhanan22@gmail.com>
2023-11-11 11:44:11 +0530

    value passing using post

--- travelpro/urls.py
+++ travelpro/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.fun, name='fun'),
-    path('add1',views.addition,name='add')
+    path('add1',views.addition,name='add'),
+    path('p_add',views.add,name='newadd')
 ]
--- travelpro/views.py
+++ travelpro/views.py
@@ -6,11 +6,18 @@ from django.shortcuts import render
 #     return HttpResponse('hello world')
 
 def fun(request):
-    return render(request, 'home.html', {'name': 'vaishnav'})
+    return render(request, 'demo_post.html', {'name': 'vaishnav'})
 
 
 def addition(request):
     n_num1 = int(request.GET['num1'])
     n_num2 = int(request.GET['num2'])
     num3 = n_num1 + n_num2
-    return render(request, 'result.html',{'value':num3})
+    return render(request, 'result.html', {'value': num3})
+
+
+def add(request):
+    num1 = int(request.POST["num1"])
+    num2 = int(request.POST["num2"])
+    num3 = num1 + num2
+    return render(request, 'demo_post2.html', {'n_value': num3})
